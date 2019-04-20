d81647ea3b5203c7637b5095344231ee503bcb66
jmpark6846 <jmpark6846@gmail.com>
2019-04-20 21:35:45 +0900

    postres db 설정

--- src/app.js
+++ src/app.js
@@ -3,7 +3,22 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import indexRouter from "./routes/index";
+import { createConnection } from 'typeorm'
+import dotenv from 'dotenv'
+dotenv.config();
 
+createConnection({
+  type:'postgres',
+  host:process.env.DB_HOST,
+  port:5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  entitySchemas: [
+      
+  ]
+})
 const app = express();
 
 app.use(logger("dev"));
